b4c4b4f508abba031773031871424bebb14200e4
Alina Ermish <alinaermish@MacBook-Air-Alina.local>
2025-04-18 00:08:01 +0700

    tot

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   const { uuid } = req.query;
   console.log('🔍 REDIRECT uuid =', uuid); // добавили лог
 
-  if (uuid) return res.status(400).send('hah UUID',req.query);
+  if (!uuid) return res.status(400).send('hah UUID',req.query);
   return res.status(400).send('hah2 UUID',req.query,req);
 
   res.writeHead(302, {
