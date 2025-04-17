619e5259bc7e87be82195419354ed271dbf5812c
Alina Ermish <alinaermish@MacBook-Air-Alina.local>
2025-04-18 00:15:57 +0700

    tot

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = (req, res) => {
   const { uuid } = req.query;
   console.log('🔍 REDIRECT uuid =', uuid); // добавили лог
 
-  if (!uuid) {return res.status(400).send('hah UUID',req.query,res);}
-  else {return res.status(400).send('hah2 UUID',req.query,req);}
+  if (!uuid) {return res.status(400).send('hah UUID,${req.query}','${res}');}
+  else {return res.status(400).send('hah2 UUID,${req.query},${req})');}
 
   res.writeHead(302, {
     Location: `/location.html?uuid=${uuid}`
