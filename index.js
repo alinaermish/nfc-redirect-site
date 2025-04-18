28e81accf26aa7013fc2a10d1194d5388ec361ba
matvilen <metell-98@mail.ru>
2025-04-18 10:52:07 +0300

    into_2_branch

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ module.exports = (req, res) => {
   const { uuid } = req.query;
   console.log('🔍 REDIRECT uuid =', uuid); // добавили лог
 
-  if (!uuid) {return res.status(400).send('hah UUID',req.query);}
-  else {return res.status(400).send('hah2 UUID,${req.query},${req})');}
+  if (!uuid) {return res.status(400).send(`hah UUID,req.query`);}
+  else {return res.status(400).send(`hah2 UUID,${req.query},${req})`);}
 
-  res.redirect('test.html');
+  res.redirect(`https://taplink.cc/ciceromaltese`);
   res.end();
 };
